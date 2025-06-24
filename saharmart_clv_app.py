fd899a0d246db80abf6c9a6d73d057d341b2e775
Bryan Waweru <mainabryan33@gmail.com>
2025-06-24 10:13:34 +0300

    Update saharmart_clv_app.py

--- saharmart_clv_app.py
+++ saharmart_clv_app.py
@@ -1,13 +1,4 @@
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 st.set_page_config(page_title="SaharMart CLV Predictor", layout="wide")
 
